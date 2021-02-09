da1f5865588ff724fcae3ce9223ce523525b1534
cristianmeza1 <78771673+cristianmeza1@users.noreply.github.com>
2021-02-08 20:23:16 -0600

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,43 @@
+var mes =prompt("ingrese el numero del mes")
+switch(mes){
+  case "1": 
+    alert("invierno")
+    break;
+  case "2":
+    alert("invierno")
+    break;
+  case "3": 
+    alert("primavera")
+    break;
+    case "4": 
+    alert("primavera")
+    break;
+  case "5":
+    alert("primavera")
+    break;
+  case "6": 
+    alert("verano")
+    break;
+    case "7": 
+    alert("verano")
+    break;
+  case "8":
+    alert("verano")
+    break;
+  case "9": 
+    alert("otoño")
+    break;
+    case "10": 
+    alert("otoño")
+    break;
+  case "11":
+    alert("otoño")
+    break;
+  case "12": 
+    alert("invierno")
+    break;
+  default:
+    alert("no es un numero de mes del año")
+    break;
+    }
+alert("Nombre:Cristian Danilo Meza Ochoa Carne:21000955")
